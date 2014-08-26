4e32bd60d8e6ee8931b6b48be7ce71174ff27fa9
slamflipstrom <lindstrom.s@gmail.com>
2014-08-26 18:07:56 -0500

    Defined separate method for string conversion

--- day1/string_calculator.rb
+++ day1/string_calculator.rb
@@ -4,22 +4,21 @@ module StringCalculator
   
     
   def self.add(numbers)
+    
     nums = numbers
     sum = 0
     if nums.empty? 
-      0
+      0 
     else
-      x=nums.split(/\W+/).map{ |x| x.to_i }
-      x.inject(:+)
+      convert_to_integer = convert_to_integer(numbers)
+      convert_to_integer.inject(:+)
     end
   end
   
-  # def self.s_to_i(string)
-#     nums.each do |p|
-#       sum = sum + p.to_i
-#     end
-#     sum
-#   end
-  
+  def self.convert_to_integer(numbers)
+    x = numbers
+    x.split(/\W+/).map{ |x| x.to_i }
+  end
+    
 end
   
